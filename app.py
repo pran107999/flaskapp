122f04871061b74542305966014c37798ea5d80e
pran107999 <praneeth.bonagiri@gmail.com>
2023-11-22 20:28:21 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, Flask!'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
